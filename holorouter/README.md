f8fd714faf2be3a559571dc3e4476382239d67ad
Burke Azbill <burkeazbill-bc@users.noreply.github.com>
2026-03-03 10:35:40 -0500

    Update for automated repo pulls and lab file placement

---
+++ holorouter/README.md
@@ -0,0 +1,3 @@
+The "startlist" is merged with the default proxy allowlist during labstartup.
+
+Once labstartup is complete, then the "allowlist" is used to restrict access to domains.
---
+++ lab-update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+. /home/holuser/.bashrc
+
+#cleanup existing files if necessary:
+
+
+# Copy lab files to Documents folder
+echo "Copying lab files to Documents/lab-files/" | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
+mkdir -p /lmchol/home/holuser/Documents/lab-files/
+cp -R /vpodrepo/2027-labs/2788/files/ /lmchol/home/holuser/Documents/lab-files/
