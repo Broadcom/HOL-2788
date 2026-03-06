a54e0129dcfc49ab5c671f9715ed8bfb06ca87c5
Burke Azbill <burkeazbill-bc@users.noreply.github.com>
2026-03-06 12:27:56 -0500

    Remove log output since it is handled by final.py in core scripts

--- lab-update.sh
+++ lab-update.sh
@@ -4,8 +4,6 @@
 
 #cleanup existing files if necessary:
 
-
 # Copy lab files to Documents folder
-echo "Copying lab files to Documents/" | tee -a /lmchol/hol/labstartup.log >> /home/holuser/hol/labstartup.log 2>&1
 mkdir -p /lmchol/home/holuser/Documents/
 cp -R /vpodrepo/2027-labs/2788/files/ /lmchol/home/holuser/Documents/
